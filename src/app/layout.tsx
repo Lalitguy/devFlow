326bd44c16edf4cc3993c0780b2614a06efb63af
Lalitguy <121784152+Lalitguy@users.noreply.github.com>
2025-04-27 21:30:40 +0530

    font setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import React from "react";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const inter = localFont({
+  src: "../fonts/InterVF.ttf",
+  variable: "--font-inter",
+  weight: "100 200 300 400 500 600 700 800 900",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const SpaceGrotesk = localFont({
+  src: "../fonts/SpaceGroteskVF.ttf",
+  variable: "--font-space-grotesk",
+  weight: "300 400 500 600 700",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-auth-dark `}
+        className={`${inter.className} ${SpaceGrotesk.variable} antialiased bg-auth-dark `}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,17 +13,10 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="font-[family-name:var(--font-geist-mono)] text-sm/6 sm:text-left text-center list-decimal list-inside">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
+        <ol>
+          <li className="font-inter">Normal Text lio1</li>
+          <li className="font-space-grotesk">Normal Text lio1</li>
+          <li>Normal Text lio1</li>
         </ol>
 
         <div className="flex sm:flex-row flex-col items-center gap-4">
