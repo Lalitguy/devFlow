5f75bc130f57ac4b21030ef7c12ea0287180e4df
Lalitguy <121784152+Lalitguy@users.noreply.github.com>
2025-05-10 16:32:08 +0530

    implemented getTags server action

--- src/app/(root)/tags/page.tsx
+++ src/app/(root)/tags/page.tsx
@@ -1,7 +1,16 @@
+import { getTags } from "@/lib/actions/tag.action";
 import React from "react";
 
-const Tags = () => {
-  return <div>Tags</div>;
+const Tags = async () => {
+  const { success, data } = await getTags({
+    page: 1,
+    pageSize: 10,
+  });
+  if (!success) return;
+  const { tags } = data || {};
+
+  console.log(JSON.stringify(tags, null, 2));
+  return <div>{JSON.stringify(tags, null, 2)}</div>;
 };
 
 export default Tags;
