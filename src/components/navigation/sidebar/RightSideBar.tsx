4ba9b068e48a42799b82b8676351434daf388104
Lalitguy <121784152+Lalitguy@users.noreply.github.com>
2025-05-11 23:21:59 +0530

    implemented hot questions

--- src/components/navigation/sidebar/RightSideBar.tsx
+++ src/components/navigation/sidebar/RightSideBar.tsx
@@ -1,89 +1,67 @@
 import TagCard from "@/components/cards/TagCard";
+import DataRenderer from "@/components/DataRenderer";
 import ROUTES from "@/constants/routes";
+import { getHotQuestions } from "@/lib/actions/question.action";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const hotQuestions = [
-  {
-    _id: "1",
-    title: "How to create and use a custom hook in React ?",
-  },
-  {
-    _id: "2",
-    title: "How to use React Query ?",
-  },
-  {
-    _id: "3",
-    title: "How to use Redux ?",
-  },
-  {
-    _id: "4",
-    title: "How to use React Router ?",
-  },
-  {
-    _id: "5",
-    title: "How to use React Context ?",
-  },
-];
+const RightSideBar = async () => {
+  const { success, data: hotQuestions, error } = await getHotQuestions();
 
-const popularTags = [
-  {
-    _id: "1",
-    name: "react",
-    questions: 100,
-  },
-  {
-    _id: "2",
-    name: "javascript",
-    questions: 200,
-  },
-  {
-    _id: "3",
-    name: "css",
-    questions: 300,
-  },
-  {
-    _id: "4",
-    name: "html",
-    questions: 400,
-  },
-  {
-    _id: "5",
-    name: "nextjs",
-    questions: 500,
-  },
-];
-
-const RightSideBar = () => {
   return (
     <section className="max-xl:hidden top-0 right-0 sticky flex flex-col gap-6 shadow-light-300 dark:shadow-none p-6 pt-32 light-border border-l w-[350px] h-screen overflow-y-auto custom-scrollbar background-light900_dark200">
       <div>
         <h3 className="text-dark200_light900 h3-bold">Top Questions</h3>
 
-        <div className="flex flex-col gap-[30px] mt-6 w-full">
-          {hotQuestions.map(({ _id, title }) => (
-            <Link
-              href={ROUTES.QUESTION(_id)}
-              key={_id}
-              className="flex justify-between items-center gap-7 cursor-pointer"
-            >
-              <p className="text-dark500_light700 body-medium">{title}</p>
-              <Image
-                src={"/icons/chevron-right.svg"}
-                alt="Chevron"
-                width={20}
-                height={20}
-                className="invert-colors"
-              />
-            </Link>
-          ))}
-        </div>
+        <DataRenderer
+          success={success}
+          error={error}
+          data={hotQuestions}
+          empty={{
+            title: "No hot questions found",
+            message:
+              "Feel free to ask your questions and they will be displayed here.",
+          }}
+          render={(hotQuestions) => (
+            <div className="flex flex-col gap-[30px] mt-7 w-full">
+              {hotQuestions.map(({ _id, title }, index) => (
+                <Link
+                  href={ROUTES.QUESTION(_id)}
+                  key={_id}
+                  className="flex justify-between items-center gap-5 cursor-pointer"
+                >
+                  <Image
+                    src={
+                      index % 2 === 0
+                        ? "/icons/hot-even.svg"
+                        : "/icons/hot-odd.svg"
+                    }
+                    alt="Hot Question"
+                    width={20}
+                    height={20}
+                    className="self-start mt-1"
+                  />
+                  <p className="text-dark500_light700 hover:underline line-clamp-2 transition-all body-medium">
+                    {title}
+                  </p>
+                  <Image
+                    src={"/icons/chevron-right.svg"}
+                    alt="Chevron"
+                    width={20}
+                    height={20}
+                    className="invert-colors"
+                  />
+                </Link>
+              ))}
+            </div>
+          )}
+        />
 
         <div className="mt-16">
           <h3 className="text-dark200_light800 h3-bold">Popular Tags</h3>
           <div className="flex flex-col gap-4 mt-7">
-            {popularTags.map(({ _id, name, questions }) => (
+            {/* {popularTags.map(({ _id, name, questions }) => (
               <TagCard
                 key={_id}
                 _id={_id}
@@ -92,7 +70,7 @@ const RightSideBar = () => {
                 showCount
                 compact
               />
-            ))}
+            ))} */}
           </div>
         </div>
       </div>
--- src/lib/actions/question.action.ts
+++ src/lib/actions/question.action.ts
@@ -14,6 +14,7 @@ import {
   PaginatedSearchParamsSchema,
 } from "../validations";
 import { auth } from "@/auth";
+import dbConnect from "../mongoose";
 
 export async function createQuestion(
   params: CreateQuestionParams
@@ -330,6 +331,23 @@ export async function incrementViews(
       data: {
         views: question.views,
       },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function getHotQuestions(): Promise<ActionResponse<QuestionI[]>> {
+  try {
+    await dbConnect();
+
+    const questions = await Question.find()
+      .sort({ views: -1, upvotes: -1 })
+      .limit(5);
+
+    return {
+      success: true,
+      data: JSON.parse(JSON.stringify(questions)),
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
