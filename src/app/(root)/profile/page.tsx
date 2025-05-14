80ea128914c3b9dfa6bb4060b856d9b509ac3a9a
Lalitguy <121784152+Lalitguy@users.noreply.github.com>
2025-05-14 21:37:08 +0530

    added profile redirect

--- src/app/(root)/profile/page.tsx
+++ src/app/(root)/profile/page.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { auth } from "@/auth";
+import ROUTES from "@/constants/routes";
+import { redirect } from "next/navigation";
 
-const Profile = () => {
-  return <div>Profile</div>;
+const Profile = async () => {
+  const session = await auth();
+
+  if (session?.user?.id) {
+    redirect(ROUTES.PROFILE(session.user.id));
+  } else {
+    redirect(ROUTES.SIGN_IN);
+  }
 };
 
 export default Profile;
