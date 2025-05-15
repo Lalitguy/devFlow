58842eb5fa9b9cc09ee3953651a34ccb72f01d31
Lalitguy <121784152+Lalitguy@users.noreply.github.com>
2025-05-15 22:53:15 +0530

    added jobs filter

---
+++ src/components/filter/JobsFilter.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { formUrlQuery } from "@/lib/url";
+
+import LocalSearch from "../search/LocalSearch";
+
+interface JobsFilterProps {
+  countriesList: Country[];
+}
+
+const JobsFilter = ({ countriesList }: JobsFilterProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const handleUpdateParams = (value: string) => {
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: "location",
+      value,
+    });
+
+    router.push(newUrl, { scroll: false });
+  };
+
+  return (
+    <div className="relative flex max-sm:flex-col justify-between sm:items-center gap-5 mt-11 w-full">
+      <LocalSearch
+        route={pathname}
+        iconPosition="left"
+        imgSrc="/icons/job-search.svg"
+        placeholder="Job Title, Company, or Keywords"
+        otherClasses="flex-1 max-sm:w-full"
+      />
+
+      <Select onValueChange={(value) => handleUpdateParams(value)}>
+        <SelectTrigger className="flex items-center gap-3 p-4 border light-border sm:max-w-[210px] min-h-[56px] text-dark500_light700 line-clamp-1 body-regular background-light800_dark300">
+          <Image
+            src="/icons/carbon-location.svg"
+            alt="location"
+            width={18}
+            height={18}
+          />
+          <div className="flex-1 text-left line-clamp-1">
+            <SelectValue placeholder="Select Location" />
+          </div>
+        </SelectTrigger>
+
+        <SelectContent className="max-w-[250px] max-h-[350px] body-semibold">
+          <SelectGroup>
+            {countriesList ? (
+              countriesList.map((country: Country) => (
+                <SelectItem
+                  key={country.name.common}
+                  value={country.name.common}
+                  className="px-4 py-3"
+                >
+                  {country.name.common}
+                </SelectItem>
+              ))
+            ) : (
+              <SelectItem value="No results found">No results found</SelectItem>
+            )}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default JobsFilter;
